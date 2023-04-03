e9baa0de5d26f7df275aef6f227ff84b12a069b8
placeparks <86215469+placeparks@users.noreply.github.com>
2023-04-03 13:38:07 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import HealthcareContract from "./components/HealthCareContract";
 
 function App() {
   return (
+    <>
+    <nav><h2 className="heading">Health Care</h2></nav>
     <div className="app-container">
       <HealthcareContract />
     </div>
+    </>
   );
 }
 
 export default App;
+
