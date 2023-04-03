ed246734faa265a0610d324a9aa4f656df6c7690
placeparks <86215469+placeparks@users.noreply.github.com>
2023-04-03 13:32:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./App.css";
 import HealthcareContract from "./components/HealthCareContract";
-import { CssBaseline, Container, AppBar, Toolbar, Typography } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Healthcare System</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <HealthcareContract />
-      </Container>
+    <div className="app-container">
+      <HealthcareContract />
     </div>
   );
 }
